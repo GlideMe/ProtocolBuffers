76a03c477d930f86445ecd375e15a5968ce3f620
Alon Genosar <alon@bootleg.co.il>
2021-07-27 13:21:47 +0300

    a

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.3
 
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "Glidetalk-iOS",
+    name: "ProtocolBuffer",
     products: [
         .library(name: "ProtocolBuffer", targets: ["ProtocolBuffer"]),
     ],
@@ -13,5 +13,12 @@ let package = Package(
             url: "https://glide-ios-jenkins-builds-02.s3.amazonaws.com/GTSDK/ProtocolBuffers/ProtocolBuffer-2.15.1.zip",
             checksum: "a7ab1e48e606d3876c6e05ce7f486d9af0992430d104733fe91bffa365eb9b8b"
         ),
+//        .target(
+//            name: "ProtocolBufferCondition",
+//            dependencies: [
+//                .target(name: "ProtocolBuffer", condition: .when(platforms: [.iOS])),
+//            ]
+//        )
     ]
 )
+
